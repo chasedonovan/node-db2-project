2c291fa209ad009e0a79bcc67f8205b8cb637a33
Chase <chasedonovan@Chases-MacBook-Pro.local>
2022-01-30 17:23:08 -0800

    minor dependency fixes

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
-// STRETCH
+const cars = [
+    {
+      vin: '42454321GEFGDSGEG',
+      make: 'lexus',
+      model: 'ls400',
+      mileage: 175000,
+      title: 'clean',
+      transmision: 'automatic'
+    },
+    {
+      vin: 'GHH234324N34R2342',
+      make: 'jeep',
+      model: 'track hawk',
+      mileage: 25000,
+      title: 'clean',
+      transmision: 'automatic'
+    },
+    {
+      vin: 'WEGT42234GSG245W4',
+      make: 'honda',
+      model: 'accord',
+      mileage: 250000,
+      title: 'salvage',
+      transmision: 'automatic'
+    }
+  ]
+
+  exports.seed = async function(knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+  } 
